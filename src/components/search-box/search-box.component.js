24c55ba61615675138bd7e1fc9c7652feeea8243
Wojciech <wojciech.wisniewski@goshop.pl>
2021-10-30 20:48:03 +0200

    adding files for github pages

---
+++ src/components/search-box/search-box.component.js
@@ -0,0 +1,9 @@
+import './search-box.styles.css';
+
+export const SearchBox = ({ placeholder, handleChange }) => (
+  <input 
+    className="search"
+    onChange={handleChange} 
+    type="search" 
+    placeholder={placeholder} />
+)
